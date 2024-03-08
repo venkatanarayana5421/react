3a7519342fa7989e9a7caee8d59331a7912163ab
venkatanarayana5421 <2200030512@kluniversity.in>
2024-03-08 20:49:49 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App({store}) {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Counseling and Visitor Management System</p>
+        <p>Counsellor </p>{/*line I changed*/}
       </header>
 
       <div className="App-body">
